7a4916398d45f10b8728c6b8d15ee5782dcf6e42
SriLalithaPallavula <38063994+SriLalithaPallavula@users.noreply.github.com>
2022-04-03 17:07:56 +0930

    Adding search and list components
    Added SearchMovie and MovieList components to search and list the results.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
+import MovieList from './components/MovieList';
+import SearchMovie from './components/SearchMovie';
 
 function App() {
+
+  const [movies, setMovies] = useState([]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="navbar">
+        <SearchMovie onSearchValue={setMovies} />
+      </div>
+      <div className="Container">
+        <div className="movieList"> <MovieList  movies={movies}/></div>
+      </div>
     </div>
   );
 }
