67111061ca7bc0aa49e6427fba27a5c84da5604f
SriLalithaPallavula <38063994+SriLalithaPallavula@users.noreply.github.com>
2022-04-05 10:58:00 +0930

    Search only if there are more than 2 characters and show error message if there are any errors.

--- src/components/SearchMovie.js
+++ src/components/SearchMovie.js
@@ -4,62 +4,13 @@ import { FaSistrix } from "react-icons/fa";
 
 const SearchMovie = (props) => {
 
-    const { setMovies,  setNumberOfResults } = props;
+    const { setResults, setError, setNumberOfResults } = props;
 
-    const [searchValue, setSearchValue] = useState(['']);
+    const [searchValue, setSearchValue] = useState('');
     const [latestSearchResults, setLatestSearchResults] = useState([]);
 
     const [yearRange, setYearRange] = useState([1950, 2022]);
-
     const [searchType, setSearchType] = useState('any');
-
-
-    const getMovieRequest = async (searchValue) => {
-        const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=f12d765f`;
-
-        const response = await fetch(url);
-        const responseJson = await response.json();
-
-        //console.log({ responseJson });
-
-        if (responseJson.Search) {
-            setLatestSearchResults(responseJson.Search);
-            setNumberOfResults(responseJson.totalResults);
-        }else {
-            setLatestSearchResults([]);
-            setNumberOfResults('');
-        }
-    };
-
-    const onYearChange = (values) => {
-        setYearRange(values);
-    };
-
-    useEffect(() => {
-        let filteredResults = [];
-
-        //filter by type
-        if (searchType === "any") {
-            filteredResults = latestSearchResults;
-        } else {
-            filteredResults = latestSearchResults.filter((result) => result.Type === searchType);
-            //   console.log({ filteredResults });
-
-        }
-
-        //filter by range
-        const minYear = yearRange[0];
-        const maxYear = yearRange[1];
-
-        filteredResults = filteredResults.filter((result) => {
-            const releaseYear = result.Year.split("–")[0]; //['2021-2022'] or ['2021']
-            return (releaseYear >= minYear && releaseYear <= maxYear);
-        });
-
-        setMovies(filteredResults);
-
-    }, [searchType, latestSearchResults, yearRange])
-
 
     const searchTypes = [
         {
@@ -80,19 +31,72 @@ const SearchMovie = (props) => {
         }
     ];
 
-    const types = (searchTypes).map((type) =>
+    const getMovieRequest = async (searchValue) => {
+        const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=f12d765f`;
 
+        const response = await fetch(url);
+        const responseJson = await response.json();
+
+        if (responseJson.Search) {
+            setError(null);
+            setLatestSearchResults(responseJson.Search);
+            setNumberOfResults(responseJson.totalResults);
+        }else {
+            if(responseJson.Error) {
+                setError(responseJson.Error);
+            }
+            setLatestSearchResults([]);
+            setNumberOfResults('');
+        }
+    };
+
+    const onYearChange = (values) => {
+        setYearRange(values);
+    };
+
+    useEffect(() => {
+        let filteredResults = [];
+
+        //filter by type
+        if (searchType === "any") {
+            filteredResults = latestSearchResults;
+        } else {
+            filteredResults = latestSearchResults.filter((result) => result.Type === searchType);
+        }
+
+        //filter by range
+        const minYear = yearRange[0];
+        const maxYear = yearRange[1];
+
+        filteredResults = filteredResults.filter((result) => {
+            const releaseYear = result.Year.split("–")[0]; //['2021-2022'] or ['2021']
+            return (releaseYear >= minYear && releaseYear <= maxYear);
+        });
+
+        setResults(filteredResults);
+
+    }, [searchType, latestSearchResults, yearRange]) 
+
+    const types = (searchTypes).map((type) =>
         <div className="radioButtonItem">
             <input id={type.value} type='radio' name='searchType' checked={type.value === searchType}
                 onChange={() => { setSearchType(type.value) }} />
             <label for={type.value}>{type.label}</label>
         </div>
-
     );
 
-    useEffect(() => {
-        getMovieRequest(searchValue);
-    }, [searchValue])
+    useEffect(() => {        
+        //Clear previous results
+        setError(null);
+        setLatestSearchResults([]);
+        setNumberOfResults('');
+
+        //Search only if there are more than 2 characters in input field
+        //otherwise api might throw 'too many results' error
+        if(searchValue.trim().length > 2) {            
+            getMovieRequest(searchValue);
+        }        
+    }, [searchValue,setError])
 
     const thumbStyle = {
         background: "#ababab",
@@ -114,6 +118,7 @@ const SearchMovie = (props) => {
         right: "-40px",
         top: "2px"
     }
+
     return (
         <div className="searchContainer">
             <div>
